131ee34302d29e86c42d851a3ed7e466125c380e
Keenan Lang <klang@aps.anl.gov>
2017-02-08 14:08:59 -0600

    Switched Allocation class to struct with constructor, allows header only definition.

--- usbApp/src/Allocation.h
+++ usbApp/src/Allocation.h
@@ -5,32 +5,37 @@
 
 #include "StorageType.h"
 
-/** Class representing a single asyn parameter */
-class Allocation
+/** Type representing a single asyn parameter */
+typedef struct Allocation
 {
-	public:
-		Allocation();
-		
-		/** Asyn Param Name */
-		std::string name;
-		
-		/** Number of Bytes to read */
-		unsigned length;
-		
-		/** Start index in usb data */
-		unsigned start;
-		
-		/** Mask for read data */
-		unsigned mask;
-		
-		/** Number of Bits to shift left or right */
-		unsigned shift;
-		
-		/** Parameter Index */
-		int index;
-		
-		/** Asyn Type to convert to */
-		STORAGE_TYPE type;
-};
+	/** Asyn Param Name */
+	std::string name;
+	
+	/** Number of Bytes to read */
+	unsigned length;
+	
+	/** Start index in usb data */
+	unsigned start;
+	
+	/** Mask for read data */
+	unsigned mask;
+	
+	/** Number of Bits to shift left or right */
+	unsigned shift;
+	
+	/** Parameter Index */
+	int index;
+	
+	/** Asyn Type to convert to */
+	STORAGE_TYPE type;
+	
+	
+	Allocation(): name(""),
+	              length(0),
+	              start(0),
+	              index(0),
+	              mask(0xFFFFFFFF),
+	              type(TYPE_UNKNOWN) {}
+} Allocation;
 
 #endif
